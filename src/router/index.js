319addb5e986bea141320180564266fc8b2964a7
ousmane-fall <fallousman06@gmail.com>
2023-02-27 16:52:45 +0100

    second commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import HistoryView from '../views/History.vue'
+import AboutView from '../views/AboutView.vue'
+
+
 
 const routes = [
   {
     path: '/',
     name: 'home-View',
     component: HomeView
+  },
+  {
+    path: '/history',
+    name: 'history-View',
+    component: HistoryView
+  },
+  
+  {
+    path: '/about',
+    name: 'About-View',
+    component: AboutView
   },
   
 ]
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>{{$t("about")}}</p>
   </div>
+  
+  
 </template>
 
 <script>
   export default {
-    name: 'about'
+    name: 'about-view'
   }
 </script>
